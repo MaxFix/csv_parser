6721221dac4f5ca30ad5ad91e3d8f01b767e858a
MadMax <max.sox@mail.ru>
2017-12-05 19:04:29 +0300

    edit find_entry function del self.flag write search aliases

--- csv_parser/storage.py
+++ csv_parser/storage.py
@@ -1,30 +1,48 @@
 class Storage(object):
 
-    lines = {}
-
-    def __init__(self, path):                               # имя файла в конструкторе path
+    def __init__(self, path):                                   # имя файла в конструкторе path
         pass
 
-    def find_entry(self, find_str,flag):                    # строка поиска
-        self.flag = False
+    def find_entry(self, sought_for, search_aliases=False):     # строка поиска
+        res = None
+        for item in Entry:                                      # item будет инстансом нового класса
+            if item.name == sought_for:                         # нужно привести в одинаковый регистр
+                res = item
+                break
+        if res:
+            return res
 
-        for find_str in lines:
+        elif search_aliases:
+            pass
 
-        if flag in self.data.keys():                        # поиск по ключу, как в Responser`e
-            print('Bot know: {}'.format(self.data[flag]))
-            return self.data[flag]
+        if search_aliases in self.data.keys():                            # поиск по ключу, как в Responser`e
+            print('Bot know: {}'.format(self.data[search_aliases]))
+            return self.data[search_aliases]
         else:
-            print('Bot do not know: {}'.format(flag))
+            print('Bot do not know: {}'.format(search_aliases))
             return None
 
-    def check_aliases(self, aliases, find_str):             # метод проверки псевдонимов
+    def check_aliases(self, aliases, find_str):                 # метод проверки псевдонимов (alias`ов)
 
-        for aliases in lines:                               # запись из словаря Storag`a
-            if aliases == find_str:                         # если псевдоним совпадает со строкой
-                find_str = True                             # найдена верная строка
+        for aliases in Entry:                                   # запись из нового класса Entry, вместо строк
+            if aliases == find_str:                             # если псевдоним совпадает со строкой
+                find_str = True                                 # найдена верная строка
                 print(find_str)
             else:
                 find_str = False
                 return None
+class Entry(object):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+        return name
+    def entry_param(self, value, name, aliases_all, aliases_sample):
+        pass
+
+
+#У него будет переопределен метод str, он будет возвращать имя.
+#Соотвественно check_aliases будет принимать экземпляры класса Entry для сравнения и строку поиска. Как у тебя сейчас.
 
 

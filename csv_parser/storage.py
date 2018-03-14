3389ec1c00e2213dab470af6f621e5f24ecab7cc
MaxFix <max.sox@mail.ru>
2018-03-14 12:25:38 +0300

    new branch

--- csv_parser/storage.py
+++ csv_parser/storage.py
@@ -1,75 +1,57 @@
-import csv
-
 class Storage(object):
 
-    data = {}
-
-    def __init__(self, path):
-        # TODO this logic will be moved to separated class
-
-        for foo in csv.reader(open(path, 'r', encoding='utf-8'), delimiter='—'):
-            food = Food(foo[0].strip().lower(), foo[1].strip().lower())
-            self.data[food.name.lower()] = food
+    def __init__(self, path):                                   # имя файла в конструкторе path
+        pass
 
     def find_entry(self, sought_for, search_aliases=False):     # строка поиска
-        """
-        :param sought_for: Искомое
-        :param search_aliases: флаг, показывает нужно ли искать в алиасах
-        :return:
-        """
-        # res = None
-        # for item in Food:                                      # item будет инстансом нового класса
-        #     if item.name == sought_for:                         # нужно привести в одинаковый регистр
-        #         res = item
-        #         break
-        # if res != item: #?
-        #     return
-        try:
-            res = self.data[sought_for.lower().strip()]
-        except KeyError:
-            res = None
+        res = None
+        for item in Entry:                                      # item будет инстансом нового класса
+            if item.name == sought_for:                         # нужно привести в одинаковый регистр
+                res = item
+                break
+        if res:
+            return res
 
-        al_res = []
-        if search_aliases == True:  # условие не полное, нужно дополнить: ищем, если установлен флаг и не нашли res
-            # искать в алиасах
+        elif search_aliases:
+            pass
 
-            al_res = self.check_aliases(sought_for)
+        if search_aliases in self.data.keys():                            # поиск по ключу, как в Responser`e
+            print('Bot know: {}'.format(self.data[search_aliases]))
+            return self.data[search_aliases]
+        else:
+            print('Bot do not know: {}'.format(search_aliases))
+            return None
 
-        return res, al_res
+    def check_aliases(self, aliases, find_str):                 # метод проверки псевдонимов (alias`ов)
 
-    def check_aliases(self, find_str, aliases):                 # метод проверки псевдонимов (alias`ов)
-
-        for aliases in []:
-            if aliases == find_str:
-                find_str = True
+        for aliases in Entry:                                   # запись из нового класса Entry, вместо строк
+            if aliases == find_str:                             # если псевдоним совпадает со строкой
+                find_str = True                                 # найдена верная строка
                 print(find_str)
             else:
                 find_str = False
-                return None            #ПЕРЕПИСАТЬ!!!
-        pass
+                return None
 
 
 class Food(object):
-    aliases_all = []
-    def __init__(self, name, value):
+    def __init__(self, value, name):
         self.value = value  # значение из предыдущей версии
         self.name = name  # ключ из 1го файла
+        #
         self._init_aliases()
 
     def __str__(self):
         return self.name
 
-        def _init_aliases(self):
+    def _init_aliases(self):
         # self.name
 
         symb = '.,<>!?/\[]()~_-=+`@:\'#$%^&*'
         name = self.name.split(" ")
         name = self.name.strip()
 
-
         for i in symb:
             name = name.replace(i, '')
 
         self.aliases_all = []
         self.aliases_all.append(name)
-        

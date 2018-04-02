f79501609a4691a4ca63dbaef6136fad8adb1081
xmx <max.sox@mail.ru>
2018-04-03 01:10:18 +0300

    I did it! All tests passed!

--- csv_parser/storage.py
+++ csv_parser/storage.py
@@ -52,10 +52,11 @@ class Food(object):
         name_c = name_c.strip().split(" ")
         self.aliases_all = name_c
 
-    def has_alias(self, sought, name_c):
-        for s in name_c:
+    def has_alias(self, sought):
+        for s in self.aliases_all:
+            print(s, sought)
             if s == sought:
                 return True
             else:
-                return False
+                False
 
